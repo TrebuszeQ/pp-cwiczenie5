262c9bff09d5de32d84ee5795895f30fbb28ab21
TrebuszeQ <trebuszeq@gmail.com>
2024-04-05 11:16:12 +0200

    zad2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <string>
+#include <utility>
 
 using namespace std;
 
-string read_input_str(string msg) {
-    string in  = "";
+string read_input_str(const string& msg) {
+    string in;
     cout << msg << endl;
     cin >> in;
 
     return in;
 }
 
-bool is_int(string str) {
+bool is_int(const string& str) {
     try { stoi(str); }
     catch (exception e) { return false; }
     return true;
 }
 
-int read_int(string msg) {
+int read_int(const string& msg) {
     string n = read_input_str(msg);
     while (true) {
         if (is_int(n)) return stoi(n);
@@ -25,25 +26,71 @@ int read_int(string msg) {
     }
 }
 
-int * read_arr(int len) {
-    int* arr = new int[len];
-    for (int i = 0; i < len; i++) { arr[i] = read_int("Wprowadz liczbe calkowita."); }
+int * read_arr_msg(int len, const string& msg) {
+    auto* arr = new int[len];
+
+    for (int i = 0; i < len; i++) { arr[i] = read_int(msg); }
     return arr;
 }
 
-int array_min(int* ptr, int len) {
-    int* arr = ptr;
-    int minn = 0;
-    for (int i = 0; i < len; i++) { minn = minn < arr[i] ? minn : arr[i]; }
+int * read_arr_msgs(int len, string* msgs) {
+    auto* arr = new int[len];
+    string* msgs_ptr = msgs;
 
-    delete[]arr;
-
-    return minn;
+    for (int i = 0; i < len; i++) { arr[i] = read_int(msgs_ptr[i]); }
+    return arr;
 }
 
-int zadanie1(int len) {
-    int* ptr = read_arr(len);
-    return array_min(ptr, len);
+int* array_min_and_count(int* ptr, int len) {
+    auto* result = new int[2];
+
+    int previous_count = 0;
+    int count = 0;
+
+    int* arr = ptr;
+    int minn = 0;
+
+    for (int i = 0; i < len; i++) {
+        if (arr[i] < minn) {
+            minn = arr[i];
+            count = 0;
+        }
+        else if (minn == arr[i]) { count++; }
+    }
+
+    delete[]arr;
+    result[0] = minn;
+    result[1] = count;
+
+    return result;
+}
+
+int* zadanie1(int len) {
+    int* ptr = read_arr_msg(len, "Wprowadz liczbe calkowita.");
+    return array_min_and_count(ptr, len);
+}
+
+int* sauce_report() {
+
+
+}
+
+string* set_sauce_msgs(char* sauces, int len) {
+    char* sauces_ptr = sauces;
+    string msgs = new string[len];
+    for (int i = 0; i < len; i++) { msgs[i] = "Wprowadz ilosc sprzedanych pojemnikow sosu " + (string) reinterpret_cast<const char *>(sauces_ptr[i]) + "."; }
+
+    return msgs;
+}
+
+int* zadanie2() {
+    int len = 5;
+    std::string* sauces[] = {"lagodny", "ostry", "kwasny", "slodki", "pikantny"};
+    auto* sales = new int[len];
+    string* msgs_ptr = set_sauce_msgs(sauces, len);
+            int* ptr = read_arr_msgs(len, );
+
+    return
 }
 
 int main() {
@@ -58,8 +105,11 @@ int main() {
 
         switch (opt) {
             case 1: {
-                int num = zadanie1(9);
-                cout << "Wartosc minimalna wynosi " << num << endl;
+                int* res_ptr = zadanie1(9);
+                cout << "Wartosc minimalna wynosi " << res_ptr[0];
+                cout << "i wystepuje " << res_ptr[1] << " razy." << endl;
+                delete[]res_ptr;
+
                 break;
             }
 
